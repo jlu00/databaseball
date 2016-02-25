a354df1886e1d9de8dc18dff82143bd8af1046a1
Thomas Dunn <tsdunn@cs.uchicago.edu>
2016-02-25 16:13:10 -0600

    taking shape

--- functions.py
+++ functions.py
@@ -6,7 +6,16 @@ import operator
 import Players
 import search_code_generator
 
-
+def compile_sample_players():
+    pos_list = ['C', 'P', '1B', '2B', '3B', 'SS', 'CF', 'LF', 'RF']
+    name_list = ['Tommy', 'Dunn', 'Jess', 'Brianna', 'The', 'Poo', 'Baseball']
+    player_list = []
+    for i in range(50):
+        first_name = name_list[((i ** 3) % len(name_list) + 2) % len(name_list)]
+        last_name = name_list[((i ** 5) % len(name_list) + 7) % len(name_list)]
+        position = pos_list[i % len(pos_list)]
+        player_list += [Players.Players(first_name, last_name, position, (1988, 2014))]
+    return player_list
 def compute_team_win_percentage(teams):
     '''
     Computes the expected win percentage for a team of players created by the user
@@ -41,7 +50,7 @@ def grab_top_pos(stats, time_period):
     pos_dict = {}
     for i in stats:
         #generate SQL query
-        stats_list = 
+        #stats_list = 
         for j in stats_list:
             if j not in rv:
                 rv[j] = 0
@@ -67,12 +76,24 @@ def grab_top_pitch(stats, params):
             rv[j] += 50 - count
     return rv
 
-def select_top_pos(pos_dict):
+def select_top_pos(sorted_pos):
     '''
     returns a dictionary 'roster' of the top players
     '''
-    return_dict = {'C': [], '1B': [], '2B': [], '3B': [], 'SS': [], 'LF': [], 'CF': [], 'RF': []}
 
-    sorted_pos = sorted(pos_dict.items(), key=operator.itemgetter(1))
-
+    #sorted_pos = sorted(pos_dict.items(), key=operator.itemgetter(1))
+    team = Players.Teams('Team')
     for i in sorted_pos:
+        if not team.roster[i.position]:
+            team.add_player(i)
+        else:
+            if decide_if_too_full(i, team):
+                team.add_player(i)
+    return team
+
+
+def decide_if_too_full(player, team):
+    slots_remaining = team.max_size - team.team_size
+    positions_remaining = len(team.roster) - team.pos_filled
+    return slots_remaining >= positions_remaining
+
